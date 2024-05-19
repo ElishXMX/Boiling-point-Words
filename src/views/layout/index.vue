20d4d3d19cb312bdef4b7c6e1f6184b7f75d725b
ElishXMX <158306376+ElishXMX@users.noreply.github.com>
2024-05-19 11:34:52 +0800

    基础框架

---
+++ src/views/layout/index.vue
@@ -0,0 +1,36 @@
+<script setup>
+  import layoutAside from '@/views/layout/components/layoutAside.vue'
+</script>
+  
+
+
+  <template>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">
+        <layoutAside />
+        <p>我是一级插槽</p>
+      </el-aside>
+      <el-main>
+         <router-view />我是二级插槽
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+  
+<style scoped lang="scss">
+.common-layout {
+  height: 100%;
+  p {
+    background-color:gray;
+    height: 100%;
+  }
+}
+
+</style>
+
+
+
+  
+  
